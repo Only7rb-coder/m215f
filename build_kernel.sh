e7cbac61247b61fa03a6e1ae756549302885d6d2
Only7rb-coder <vamp.zodiac@gmail.com>
2025-02-26 18:18:15 -0500

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 export ARCH=arm64
-export PLATFORM_VERSION=12
-export ANDROID_MAJOR_VERSION=s
+mkdir out
 
+BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
+CLANG_TRIPLE=aarch64-linux-gnu-
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make ARCH=arm64 CONFIG_SECTION_MISMATCH_WARN_ONLY=y exynos9610-m21dd_defconfig
-make ARCH=arm64 CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE exynos9610-m21dd_defconfig
+make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+ 
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
